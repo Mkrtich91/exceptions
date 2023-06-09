21f2ad2d94295da326e369ef742f1c8310229e91
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-06-09 16:20:29 +0400

    solved at 09.06 m

--- Exceptions/ThrowingArgumentNull.cs
+++ Exceptions/ThrowingArgumentNull.cs
@@ -1,49 +1,104 @@
-﻿namespace Exceptions
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace Exceptions
 {
     public static class ThrowingArgumentNull
     {
         public static bool CheckParameterAndThrowException1(object o)
         {
-            // TODO 2-1. Add the code to throw the ArgumentNullException if the o argument is null.
+            if (o == null)
+            {
+                throw new ArgumentNullException(nameof(o));
+            }
+
             return true;
         }
 
         public static bool CheckParametersAndThrowException2(object o1, object o2)
         {
-            // TODO 2-2. Add the code to throw the ArgumentNullException if the o1 or o2 argument is null.
+            if (o1 == null)
+            {
+                throw new ArgumentNullException(nameof(o1));
+            }
+
+            if (o2 == null)
+            {
+                throw new ArgumentNullException(nameof(o2));
+            }
+
             return true;
         }
 
         public static int CheckParametersAndThrowException3(int[] integers, long[] longs, float[] floats)
         {
-            // TODO 2-3. Add the code to throw the ArgumentNullException if the integers, longs or floats argument is null.
+            if (integers == null)
+            {
+                throw new ArgumentNullException(nameof(integers));
+            }
+
+            if (longs == null)
+            {
+                throw new ArgumentNullException(nameof(longs));
+            }
+
+            if (floats == null)
+            {
+                throw new ArgumentNullException(nameof(floats));
+            }
+
             return integers.Length + longs.Length + floats.Length;
         }
 
         public static int CheckParameterAndThrowException4(string s)
         {
-            // TODO 2-4. Add the code to throw the ArgumentNullException if the s argument is null.
+            if (s == null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+
             return s.Length;
         }
 
         public static int CheckParametersAndThrowException5(string s1, string s2)
         {
-            // TODO 2-5. Add the code to throw the ArgumentNullException if the s1 or s2 argument is null.
+            if (s1 == null)
+            {
+                throw new ArgumentNullException(nameof(s1));
+            }
+
+            if (s2 == null)
+            {
+                throw new ArgumentNullException(nameof(s2));
+            }
+
             return s1.Length + s2.Length;
         }
 
         public static int CheckParametersAndThrowException6(string s, int[] integers, string[] strings)
         {
-            // TODO 2-6. Add the code to throw the ArgumentNullException if the s, integers or strings argument is null.
+            if (s == null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+
+            if (integers == null)
+            {
+                throw new ArgumentNullException(nameof(integers));
+            }
+
+            if (strings == null)
+            {
+                throw new ArgumentNullException(nameof(strings));
+            }
+
             return s.Length + integers.Length + strings.Length;
         }
 
         public static int CheckParameterAndThrowException7(int[] integers)
         {
             int integersCount;
-
-            // TODO 2-7. Add the null-coalescing operator to throw the ArgumentNullException if the integers argument is null.
-            integersCount = integers.Length;
+            integersCount = integers?.Length ?? throw new ArgumentNullException(nameof(integers), "The 'integers' argument cannot be null.");
 
             return integersCount;
         }
@@ -51,8 +106,16 @@
         public static int CheckParametersAndThrowException8(int[] integers, string s)
         {
             int integersCount, stringLength;
+            if (integers == null)
+            {
+                throw new ArgumentNullException(nameof(integers));
+            }
 
-            // TODO 2-8. Add the null-coalescing operator to throw the ArgumentNullException if the integers or s argument is null.
+            if (s == null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+
             integersCount = integers.Length;
             stringLength = s.Length;
 
@@ -63,7 +126,26 @@
         {
             int floatsCount, s1Length, doublesCount, s2Length;
 
-            // TODO 2-9. Add the null-coalescing operator to throw the ArgumentNullException if the any method argument is null.
+            if (floats == null)
+            {
+                throw new ArgumentNullException(nameof(floats));
+            }
+
+            if (s1 == null)
+            {
+                throw new ArgumentNullException(nameof(s1));
+            }
+
+            if (doubles == null)
+            {
+                throw new ArgumentNullException(nameof(doubles));
+            }
+
+            if (s2 == null)
+            {
+                throw new ArgumentNullException(nameof(s2));
+            }
+
             floatsCount = floats.Length;
             doublesCount = doubles.Length;
             s1Length = s1.Length;
